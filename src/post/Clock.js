f0d16591dc08fd807bc4bb1a7fc8566e0ff56305
Wenxin Liu <wenxin.liu@springernature.com>
2021-09-28 12:35:27 +0100

    Set timer takes both minutes and seconds

--- src/post/Clock.js
+++ src/post/Clock.js
@@ -6,14 +6,19 @@ import './youtube.css'
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', time: {}, seconds: 0};
+    this.state = {
+      inputMinutes: 0,
+      inputSeconds: 0,
+      time: {},
+      seconds: 0
+    };
     this.timer = [];
     this.startTimer = this.startTimer.bind(this);
     this.countDown = this.countDown.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  secondsToTime(secs){
+  secondsToTime(secs) {
     let hours = Math.floor(secs / (60 * 60));
 
     let divisor_for_minutes = secs % (60 * 60);
@@ -32,15 +37,21 @@ class Clock extends React.Component {
 
   componentDidMount() {
     let timeLeftVar = this.secondsToTime(this.state.seconds);
-    this.setState({ time: timeLeftVar });
+    this.setState({time: timeLeftVar});
   }
 
   startTimer(event) {
-    this.state.seconds = this.state.value;
+    let seconds = 0;
+    seconds += parseInt(this.state.inputMinutes) * 60 + parseInt(this.state.inputSeconds);
 
-    if (this.state.seconds > 0) {
-      this.timer.push(setInterval(this.countDown, 1000));
-    }
+    this.setState({
+      seconds: seconds
+    }, () => {
+      if (this.state.seconds > 0) {
+        this.timer.push(setInterval(this.countDown, 1000));
+      }
+    });
+
     event.preventDefault();
   }
 
@@ -49,41 +60,46 @@ class Clock extends React.Component {
     console.log("this.timer value " + this.timer);
 
     if (this.timer.length > 1) {
-      for (let i = 0; i < this.timer.length-1; i++) {
+      for (let i = 0; i < this.timer.length - 1; i++) {
         clearInterval(this.timer[i]);
       }
     }
 
     let seconds = this.state.seconds - 1;
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
-    });
+    this.setState({time: this.secondsToTime(seconds), seconds: seconds});
 
     // Check if we're at zero.
     if (seconds <= 0) {
-      clearInterval(this.timer[this.timer.length-1]);
+      clearInterval(this.timer[this.timer.length - 1]);
       alert("Your time is up!");
     }
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+
   }
 
   render() {
-    return(
-      <div id="Clock">
-        <form onSubmit={this.startTimer}>
-          <label>
-            Time in seconds:
-            <input type="text" onChange={this.handleChange} />
-          </label>
-          <input type="submit" value="Start timer" />
-        </form>
-        minutes: {this.state.time.m} seconds: {this.state.time.s}
-      </div>
-    );
+    return (<div id="Clock">
+      Set timer?
+      <form onSubmit={this.startTimer}>
+        <label>
+          Minutes:
+          <input type="text" name="inputMinutes" onChange={this.handleChange}/>
+        </label>
+        <label>
+          Seconds:
+          <input type="text" name="inputSeconds" onChange={this.handleChange}/>
+        </label>
+        <input type="submit" value="Start timer"/>
+      </form>
+      Time left:
+      minutes: {this.state.time.m}
+      seconds: {this.state.time.s}
+    </div>);
   }
 }
 export default Clock;
