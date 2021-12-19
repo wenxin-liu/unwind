6bcbe571d0bf48c62b11868206ba98ed20730180
Wenxin Liu <wenxin.liu@springernature.com>
2021-12-19 23:39:20 +0000

    Add a start button on the landing page
    Also improve styling of landing page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import './index.css'
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import './index.css';
+
+function startVideo() {
+  console.log("button clicked");
+  return ""
+}
 
 const Home = () => {
   return (
     <div className="HeroContainer">
       <p className="pre-text">Let's go to...</p>
       <p className="location">Maldives?</p>
+      <Button
+        variant="contained"
+        id="start-button"
+        onClick={startVideo()}>
+        START
+      </Button>
     </div>)
 };
 
