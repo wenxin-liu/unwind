320c7f42277203a18cb87272f19eeca42d80a171
Wenxin Liu <wenxin.liu@springernature.com>
2021-12-20 01:55:58 +0000

    Improve styling of landing page, center elements on all screen sizes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,19 @@ function startVideo() {
 
 const Home = () => {
   return (
-    <div className="HeroContainer">
-      <p className="pre-text">Let's go to...</p>
-      <p className="location">Maldives?</p>
-      <Button
-        variant="contained"
-        id="start-button"
-        onClick={startVideo()}>
-        START
-      </Button>
+    <div id="landing-page-flex-container">
+      <div id="landing-page-flex-item-text">
+        <p id="pre-text">Let's go to...</p>
+        <p id="location">Maldives?</p>
+      </div>
+      <div id="landing-page-flex-item-button">
+        <Button
+          variant="contained"
+          id="start-button"
+          onClick={startVideo()}>
+          START
+        </Button>
+      </div>
     </div>)
 };
 
